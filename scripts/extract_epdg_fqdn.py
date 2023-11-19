190cc34b053b4e33251047a7aeb9349eabe81e46
Gabriel Gegenhuber <ggegenhuber@sba-research.org>
2023-11-19 02:34:35 +0100

    add generation of cname list when extracting vowifi fqdns

--- scripts/extract_epdg_fqdn.py
+++ scripts/extract_epdg_fqdn.py
@@ -4,9 +4,10 @@ import json
 import logging
 import os
 import sys
+import re
 
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from defusedxml import ElementTree as ET
 
@@ -14,6 +15,15 @@ from mbntools.mbn import Mbn
 from mbntools.mcfg import MCFG_Item
 
 logger = logging.getLogger(__name__)
+
+REGEX_VOWIFI = "^epdg.epc.mnc(\d{2,3}).mcc(\d{3}).pub.3gppnetwork.org\.?$"
+def is_3gpp_vowifi_domain(host):
+    return re.search(REGEX_VOWIFI, host)
+
+def create_3gpp_vowifi_domain(mcc, mnc):
+    return f"epdg.epc.mnc{mnc:03d}.mcc{mcc:03d}.pub.3gppnetwork.org"
+
+#epdg.epc.mnc027.mcc208.pub.3gppnetwork.org. CNAME epdgc.epc.mnc027.mcc208.pub.3gppnetwork.org.
 
 def usage():
     print(f"Usage: {sys.argv[0]} <out-file> <dir>", file=sys.stderr)
@@ -24,6 +34,7 @@ def main():
         usage()
 
     fqdns = {}
+    cname_mapping = []
 
     for (dirpath, _, filenames) in os.walk(sys.argv[2]):
         dpath = Path(dirpath)
@@ -32,21 +43,27 @@ def main():
                 continue
 
             fp = dpath / f
-            fqdn = extract(fp)
+
+            # build json
+            fqdn = extract_iwlan_fqdns(fp)
             if fqdn is not None:
                 fqdns.setdefault(fqdn, []).append(str(fp))
             else:
                 fqdns.setdefault("", []).append(str(fp))
+            
+            # build cname mapping
+            cname_mapping.extend(extract_cname_mapping(fp))
 
-    with open(sys.argv[1], "x") as f:
+    # dump json
+    with open(f"{sys.argv[1]}.json", "x") as f:
         json.dump(fqdns, f, indent=2)
 
-def extract(path: Path) -> Optional[str]:
-    with open(path, "rb") as f:
-        mbn = Mbn(f)
+    # dump list
+    with open(f"{sys.argv[1]}.txt", "x") as f:
+        f.writelines(line + '\n' for line in cname_mapping)
 
+def extract_iwlan_fqdns_from_mbn(path: Path, mbn: Mbn) -> Optional[str]:
     items = list(filter(nonempty_file, mbn["mcfg"].get_file_items(b"/data/iwlan_s2b_config.xml\x00")))
-
     if len(items) == 0:
         logger.warning(f"Failed to find 'iwlan_s2b_config.xml' in '{path}'.")
         return None
@@ -78,6 +95,32 @@ def extract(path: Path) -> Optional[str]:
         raise NotImplementedError
 
     return fqdns.pop() if len(fqdns) == 1 else None
+
+def extract_iwlan_fqdns(path: Path) -> Optional[str]:
+    with open(path, "rb") as f:
+        mbn = Mbn(f)
+    return extract_iwlan_fqdns_from_mbn(path, mbn)
+     
+
+def generate_cname_mapping_for_fqdns(mbn: Mbn, fqdns: str) -> List[str]:
+    cname_mapping = []
+    mnoids = mbn["mcfg"]["trailer"]["mnoid"].get('ids',[])
+    for d in mnoids:
+        standardized_domain = create_3gpp_vowifi_domain(d.mcc, d.mnc)
+        cname_mapping.append(f"{standardized_domain} CNAME {fqdns}")
+    return cname_mapping
+
+def extract_cname_mapping(path: Path) -> List[str]:
+    with open(path, "rb") as f:
+        mbn = Mbn(f)
+    fqdns = extract_iwlan_fqdns_from_mbn(path, mbn)
+    if fqdns is not None:
+        if not is_3gpp_vowifi_domain(fqdns):
+            return generate_cname_mapping_for_fqdns(mbn, fqdns)
+        else:
+            logger.warning(f"Ignore 3gpp standardized fqdns {fqdns}")
+    return []
+    
 
 def nonempty_file(i: MCFG_Item) -> bool:
     return len(i["data"]) != 0 and (i["data"][0] != 7 or len(i["data"]) > 1)
